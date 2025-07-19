1bb4abfe720eda0040e0ade7dccd700fe95b22bd
Chris Muga <ChrisMuga94@Gmail.com>
2025-07-19 17:47:42 +0300

    Dereferencing

--- main.c
+++ main.c
@@ -5,11 +5,17 @@ int main() {
 	int p;
 
 	p = 1000;
+	int *q;
 
 	// Printing the pointer/address of a variable
 	printf("%p\n", &p);
 	// Printing and integer variable
 	printf("%d\n", p);
+	// q takes the address of p;
+	q = &p;
+	// Derereferencing p (1000) and re-assigning it to the new value (3000)
+	*q = 3000;
+	printf("%d\n", *q);
 
 	return 0;
 }
