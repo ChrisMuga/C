d76948b23c1a55048e8a87efe273e3ca5f8a5539
Chris Muga <ChrisMuga94@Gmail.com>
2025-07-19 17:29:30 +0300

    main file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int p;
+
+	p = 1000;
+
+	// Printing the pointer/address of a variable
+	printf("%p\n", &p);
+	// Printing and integer variable
+	printf("%d\n", p);
+
+	return 0;
+}
